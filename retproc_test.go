f99048dd07dadca96cfd418ce0f2fcef2b1a48d2
neverlee <neverlea@formail.com>
2023-06-19 13:23:22 +0800

    add a retproc unit test about panic

--- retproc_test.go
+++ retproc_test.go
@@ -96,63 +96,26 @@ func Test_RetPorc_FalseReturn(t *testing.T) {
 	}
 }
 
+func Test_RetProc_Panic(t *testing.T) {
+	p := Proc{}
+	assert.Panics(t, func() {
+		p.Do(func() {
+			panic("panic")
+		})
+	})
+	var f func(a int) (int, error)
+	{
+		rp := GetProcOfFunc1r2(f)
+		k := 0
+		assert.Panics(t, func() {
+			rp.Dow(func() {
+				defer func() {
+					k = 9
+				}()
+				panic("hello")
+			})
+		})
 
-// func Test_RetProc_Return(t *testing.T) {
-// 	p := Proc{}
-// 	assert.NotPanics(t, func() {
-// 		p.Do(func() {
-// 			p.ReturnOnly()
-// 		})
-// 	})
-// 
-// 	assert.NotPanics(t, func() {
-// 		p.Do(func() {
-// 			p.ReturnWith(nil)
-// 		})
-// 	})
-// 
-// 	assert.NotPanics(t, func() {
-// 		p.Do(func() {
-// 			p.IfReturnWith(true, nil)
-// 		})
-// 	})
-// }
-// 
-// 
-// func Test_RetProc_Panic(t *testing.T) {
-// 	p := Proc{}
-// 	assert.Panics(t, func() {
-// 		p.Do(func() {
-// 			panic("panic")
-// 		})
-// 	})
-// }
-// 
-// func Test_RetProc_ReturnFunc(t *testing.T) {
-// 	p := Proc{}
-// 	a := 9
-// 	p.Do(func() {
-// 		p.ReturnWith(func() {
-// 			a = 100
-// 		})
-// 	})
-// 	assert.Equal(t, 100, a)
-// 
-// 	p.Do(func() {
-// 		p.IfReturnWith(true, func() {
-// 			a = 200
-// 		})
-// 		a = 250
-// 	})
-// 	assert.Equal(t, 200, a)
-// 
-// 	p.Do(func() {
-// 		p.IfReturnWith(false, func() {
-// 			a = 300
-// 		})
-// 		a = 350
-// 	})
-// 	assert.Equal(t, 350, a)
-// }
-
-
+		assert.Equal(t, k, 9)
+	}
+}
